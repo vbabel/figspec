ca6a1a78aad3ac369ff7bf07bdd6c6cc00c569dd
Victor Babel <victor@chromatic.com>
2025-12-01 14:18:58 -0800

    add built files

---
+++ esm/es2019/ui/styles.js
@@ -0,0 +1,19 @@
+import { styles as empty } from "./empty/empty.js";
+import { styles as fullscreenPanel } from "./fullscreenPanel/fullscreenPanel.js";
+import { styles as iconButton } from "./iconButton/iconButton.js";
+import { styles as infoItems } from "./infoItems/infoItems.js";
+import { styles as inspectorPanel } from "./inspectorPanel/inspectorPanel.js";
+import { styles as menuBar } from "./menuBar/menuBar.js";
+import { styles as preferencesPanel } from "./preferencesPanel/preferencesPanel.js";
+import { styles as selectBox } from "./selectBox/selectBox.js";
+import { styles as snackbar } from "./snackbar/snackbar.js";
+export const styles =
+  inspectorPanel +
+  selectBox +
+  iconButton +
+  fullscreenPanel +
+  menuBar +
+  infoItems +
+  empty +
+  preferencesPanel +
+  snackbar;
